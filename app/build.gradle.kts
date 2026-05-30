fd8d626f459ea21c14de26b86291e278721d1619
Sam S. Yu <25761223+yudataguy@users.noreply.github.com>
2026-05-30 14:27:28 -0700

    chore: align JVM compile targets

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,6 +22,15 @@ android {
     composeOptions {
         kotlinCompilerExtensionVersion = "1.5.15"
     }
+
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
 }
 
 dependencies {
